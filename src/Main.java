a47a998002cb487dfc9d12093bd45980743524e6
Alec Condry <aleccondry@gmail.com>
2018-06-01 13:37:44 -0400

    jpanel

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JPanel {
     public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 1000;
+
+
+    public Main(){
+
+    }
+
+    public void PaintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+    }
 
     //sets ups the panel and frame.
     public static void main(String[] args) {
@@ -9,7 +21,6 @@ public class Main extends JPanel {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
 
-//        while(true) {
         Main panel = new Main();
         panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
 
@@ -20,6 +31,5 @@ public class Main extends JPanel {
         window.add(panel);
         window.setVisible(true);
         window.setResizable(false);
-//        }
     }
 }
