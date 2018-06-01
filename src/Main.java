f2650d153f82cef89f58b3651bc8605dae85c579
Alec Condry <aleccondry@gmail.com>
2018-06-01 13:30:01 -0400

    jpanel

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
-public class Main {
+import javax.swing.*;
 
+public class Main extends JPanel {
+    public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 1000;
+
+    //sets ups the panel and frame.
     public static void main(String[] args) {
-        System.out.println("KappaPride dude");
-        //commit michael
+        JFrame window = new JFrame("Chess!");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+
+//        while(true) {
+        Main panel = new Main();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
+//        }
     }
 }
