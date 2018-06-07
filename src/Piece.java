4eda1daf5ea51e22fc060fb165de6d1a770a3ba9
Alec Condry <aleccondry@gmail.com>
2018-06-07 13:16:50 -0400

    move class complete

--- src/Piece.java
+++ src/Piece.java
@@ -9,44 +9,145 @@ public class Piece {
     private int x, y;
     private String id;
     private boolean color;
+    public static int nextID = 1;
+    private int myID;
+    private boolean pawnMoved;
 
     public Piece(int x, int y, String id, boolean color) {
         this.x = x;
         this.y = y;
         this.id = id;
         this.color = color;
+        myID = nextID;
+        nextID++;
+    }
+
+    public boolean equals(Piece other){
+        return myID == other.myID;
     }
 
     public void move(int movetox, int movetoy, ArrayList<Piece> pieces) {
-        if (color){ //if white
-            for (int i = 0; i < pieces.size(); i++) {
-                if (pieces.get(i).getX() == movetox && pieces.get(i).getY() == movetoy) {
-                    if (!pieces.get(i).isColor()){
-                        pieces.remove(i);
-                        i--;
-                        x = movetox;
-                        y = movetoy;
-                    }
-                }
-                else{
+        int ogX = x;
+        int ogY = y;
+
+        if (id.equals("RookWhite") || id.equals("RookBlack")) { //rook movement
+            if (movetoy == y || movetox == x) {//if moving straight up or down or left or right
+                x = movetox;
+                y = movetoy;
+            }
+        }
+        else if (id.equals("BishopWhite") || id.equals("BishopBlack")) { //bishop movement
+            for (int i = -8; i < 8; i++) {
+                if ((movetoy == y+i && movetox == x+i) || (movetoy == y-i && movetox == x+i)) {//if moving diagonal
                     x = movetox;
                     y = movetoy;
                 }
             }
         }
-        if (!color){ //if black
-            for (int i = 0; i < pieces.size(); i++) {
-                if (pieces.get(i).getX() == movetox && pieces.get(i).getY() == movetoy) {
-                    if (pieces.get(i).isColor()){
-                        pieces.remove(i);
-                        i--;
+        else if (id.equals("KnightWhite") || id.equals("KnightBlack")) { //rook movement
+            if ((movetoy == y+2 && movetox == x+1)||(movetoy == y-2 && movetox == x+1)||(movetoy == y+2 && movetox == x-1)||(movetoy == y-2 && movetox == x-1)) {//L movement
+                x = movetox;
+                y = movetoy;
+                }
+            else if ((movetoy == y+1 && movetox == x+2)||(movetoy == y-1 && movetox == x+2)||(movetoy == y+1 && movetox == x-2)||(movetoy == y-1 && movetox == x-2)) {//L movement
+                x = movetox;
+                y = movetoy;
+            }
+        }
+        else if (id.equals("PawnWhite")){ //pawn white movement
+            if ((movetox == x && movetoy == y-1) ||(movetox == x && movetoy == y-2 && !pawnMoved)){
+                x = movetox;
+                y = movetoy;
+                pawnMoved = true;
+            }
+            else if ((movetox == x+1 && movetoy == y-1)||(movetox == x-1 && movetoy == y-1)){
+                for (int i = 0; i < pieces.size(); i++) { //Attacking black
+                    if (!equals(pieces.get(i))) {
+                        if (pieces.get(i).x == movetox && pieces.get(i).y == movetoy && !pieces.get(i).color) {
+                            x = movetox;
+                            y = movetoy;
+                        }
+                    }
+                }
+            }
+            if (y == 0){
+                id = "QueenWhite";
+            }
+        }
+        else if (id.equals("PawnBlack")){ //pawn black movement
+            if ((movetox == x && movetoy == y+1) ||(movetox == x && movetoy == y+2 && !pawnMoved)){
+                x = movetox;
+                y = movetoy;
+                pawnMoved = true;
+            }
+            else if ((movetox == x+1 && movetoy == y+1)||(movetox == x-1 && movetoy == y+1)){
+                for (int i = 0; i < pieces.size(); i++) { //Attacking white
+                    if (!equals(pieces.get(i))) {
+                        if (pieces.get(i).x == movetox && pieces.get(i).y == movetoy && pieces.get(i).color) {
+                            x = movetox;
+                            y = movetoy;
+                        }
+                    }
+                }
+            }
+            if (y == 7){
+                id = "Latifa";
+            }
+        }
+        else if (id.equals("QueenWhite") || id.equals("Latifa")){ //queen movement
+            if (movetoy == y || movetox == x) {//if moving straight up or down or left or right
+                x = movetox;
+                y = movetoy;
+            }
+            for (int i = -8; i < 8; i++) {
+                if ((movetoy == y+i && movetox == x+i) || (movetoy == y-i && movetox == x+i)) {//if moving diagonal
+                    x = movetox;
+                    y = movetoy;
+                }
+            }
+        }
+        else if (id.equals("KingWhite") || id.equals("KingBlack")){ //king movement
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if ((movetoy == y+i && movetox == x+j)){
                         x = movetox;
                         y = movetoy;
                     }
                 }
-                else{
-                    x = movetox;
-                    y = movetoy;
+
+            }
+        }
+        else{
+            x = movetox;
+            y = movetoy;
+        }
+        if (color){ //if white
+            for (int i = 0; i < pieces.size(); i++) { //Attacking black
+                if (!equals(pieces.get(i))){//moves back if white in that square (may take a turn later watch out)
+                    if (pieces.get(i).getX() == movetox && pieces.get(i).getY() == movetoy && pieces.get(i).isColor()) {
+                        x = ogX;
+                        y = ogY;
+                        break;
+                    }
+                }
+                if (pieces.get(i).getX() == x && pieces.get(i).getY() == y && !pieces.get(i).isColor()) {
+                    pieces.remove(i);
+                    i--;
+                }
+            }
+        }
+        if (!color){ //if black
+            for (int i = 0; i < pieces.size(); i++) { //Attacking white
+                if (!equals(pieces.get(i))){//moves back if black in that square
+                    if (pieces.get(i).getX() == movetox && pieces.get(i).getY() == movetoy && !pieces.get(i).isColor()) {
+                        x = ogX;
+                        y = ogY;
+                        break;
+                    }
+                }
+                if (pieces.get(i).getX() == x && pieces.get(i).getY() == y && pieces.get(i).isColor()) {
+                    pieces.remove(i);
+                    i--;
                 }
             }
         }
@@ -85,4 +186,5 @@ public class Piece {
     public String toString() {
         return x + " " + y + " " + id;
     }
+
 }
